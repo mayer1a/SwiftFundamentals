87e4f103cc29d9f7c35a7910f0e6735d9f363b92
Artem Mayer <46002217+mayer1a@users.noreply.github.com>
2022-02-06 20:07:28 +0700

    Внесение правок по print() и переменным

--- Lesson1/Task1/Task1/main.swift
+++ Lesson1/Task1/Task1/main.swift
@@ -11,27 +11,35 @@ import Foundation
 let aCoef: Double = 4
 let bCoef: Double = 16
 let cCoef: Double = 10.65
+let resultMessage: String
 
 // Вычисляем дискриминант
-var discriminant: Double = pow(bCoef, 2.0) - 4 * aCoef * cCoef
+let discriminant: Double = pow(bCoef, 2.0) - 4 * aCoef * cCoef
 
 // Проверяем значение дискриминанта на количество решений квадратного уравнения
 //    затем вычисляем корни уравнения, если таковые имеются
 if discriminant < Double(0) {
-	print(String(format: "Квадратное уравнение не имеет решений, "
-					+ "так как дискриминант < 0 (D = %.3f)", discriminant))
-}
-else if discriminant == Double(0) {
+	resultMessage = String(format: "Квадратное уравнение не имеет решений, "
+						   + "так как дискриминант < 0 (D = %.3f)", discriminant)
+
+	print(resultMessage)
+
+} else if discriminant == Double(0) {
 	let x1Sqr: Double = (-bCoef) / (2 * aCoef)
-	
-	print(String(format: "Уравнение имеет единственный корень x1 = %.3f, "
-					+ "так как дискриминант = 0 (D = %.3f)", x1Sqr, discriminant))
-}
-else {
+
+	resultMessage = String(format: "Уравнение имеет единственный корень x1 = %.3f, "
+						   + "так как дискриминант = 0 (D = %.3f)", x1Sqr, discriminant)
+
+	print(resultMessage)
+
+} else {
 	let x1Sqr: Double = ((-bCoef) + sqrt(discriminant)) / (2 * aCoef)
 	let x2Sqr: Double = ((-bCoef) - sqrt(discriminant)) / (2 * aCoef)
+
+	resultMessage = String(format: "Уравнение имеет корни x1 = %.3f и x2 = %.3f, "
+						   + "так как дискриминант > 0 (D = %.3f)", x1Sqr, x2Sqr, discriminant)
 	
-	print(String(format: "Уравнение имеет корни x1 = %.3f и x2 = %.3f, "
-					+ "так как дискриминант > 0 (D = %.3f)", x1Sqr, x2Sqr, discriminant))
+	print(resultMessage)
+
 }
 
